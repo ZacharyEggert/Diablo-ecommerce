01ee6190d20bf73ace0c808b630fc2886545247e
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-12-13 17:27:02 -0800

    delete server.

--- server/models/index.ts
+++
@@ -1,6 +0,0 @@
-import Reverb from './reverb';
-import Item from './item';
-import User from './user';
-
-
-export { Item, Reverb, User };
--- server/paths.ts
+++
@@ -1,8 +0,0 @@
-import 'module-alias/register'
-import { addAliases } from 'module-alias'
-
-addAliases({
-    "@controllers": `${__dirname}/controllers/`,
-    "@models": `${__dirname}/models/`,
-    "@routes": `${__dirname}/routes/`
-})
