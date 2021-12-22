79aa16031db26b9db59012f0d73a568d6d4de141
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-12-22 15:32:08 -0800

    urql codegen

--- src/components/ProductTileView.tsx
+++ src/components/ProductTileView.tsx
@@ -1,8 +1,20 @@
-import { Product } from '@lib/types';
+// import { Product } from '@lib/types';
 import { FC } from 'react';
 
 interface TileViewProps {
-    products: Product[];
+    products: {
+        __typename?: 'Listing';
+        title: string;
+        description: string;
+        finish?: string | null | undefined;
+        model: string;
+        submodel?: string | null | undefined;
+        imageUrls: Array<string>;
+        price: number;
+        year?: number | null | undefined;
+        make: string;
+        id: number;
+    }[];
 }
 
 const ProductTileView: FC<TileViewProps> = ({ products }) => {
@@ -15,17 +27,17 @@ const ProductTileView: FC<TileViewProps> = ({ products }) => {
                     {products.map((product) => (
                         <a
                             key={product.id}
-                            href={product.href}
+                            href={'/product/' + product.id}
                             className='group'>
                             <div className='w-full overflow-hidden bg-gray-200 rounded-lg xl:aspect-w-7 xl:aspect-h-8 aspect-square'>
                                 <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
+                                    src={product.imageUrls[0]}
+                                    alt={product.title}
                                     className='object-cover object-center w-full h-full group-hover:opacity-90'
                                 />
                             </div>
                             <h3 className='mt-4 text-sm text-skin-text'>
-                                {product.name}
+                                {product.title}
                             </h3>
                             <p className='mt-1 text-lg font-medium text-skin-primary'>
                                 {product.price}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,71 +4,78 @@ import ProductTileView from '@components/ProductTileView';
 import { useProductFetch } from '@lib/api/test.products/hooks';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { useListingsQuery } from 'src/generated/graphql';
 
 export default function Home() {
-    const router = useRouter();
-    const [page, setPage] = useState(1);
+    // const router = useRouter();
+    // const [page, setPage] = useState(1);
 
     const [isLoading, setIsLoading] = useState(true);
 
-    const {
-        data,
-        isLoading: isLoadingFetch,
-        error,
-        firePromise: fetchProducts,
-        pageCount,
-        getPageCount,
-    } = useProductFetch();
+    const [{ data, error }, call] = useListingsQuery();
 
     useEffect(() => {
-        console.log('Home page rendered');
-        if (router.query.page) {
-            if (Array.isArray(router.query.page)) {
-                setPage(parseInt(router.query.page[0]));
-            } else {
-                setPage(parseInt(router.query.page));
-            }
-        }
-    }, [router.query]);
+        call();
+        setIsLoading(false);
+    }, []);
+    // const {
+    //     data,
+    //     isLoading: isLoadingFetch,
+    //     error,
+    //     firePromise: fetchProducts,
+    //     pageCount,
+    //     getPageCount,
+    // } = useProductFetch();
 
-    const changePage = (page: number) => {
-        window.history.replaceState(null, 'Page ' + page, `/?page=${page}`);
-        setPage(page);
-    };
+    // useEffect(() => {
+    //     console.log('Home page rendered');
+    //     if (router.query.page) {
+    //         if (Array.isArray(router.query.page)) {
+    //             setPage(parseInt(router.query.page[0]));
+    //         } else {
+    //             setPage(parseInt(router.query.page));
+    //         }
+    //     }
+    // }, [router.query]);
 
-    useEffect(() => {
-        if (fetchProducts) {
-            fetchProducts(page);
-        }
-        if (getPageCount) {
-            getPageCount();
-        }
-    }, [page]);
+    // const changePage = (page: number) => {
+    //     window.history.replaceState(null, 'Page ' + page, `/?page=${page}`);
+    //     setPage(page);
+    // };
 
-    useEffect(() => {
-        setIsLoading(isLoadingFetch);
-    }, [isLoadingFetch]);
+    // useEffect(() => {
+    //     if (fetchProducts) {
+    //         fetchProducts(page);
+    //     }
+    //     if (getPageCount) {
+    //         getPageCount();
+    //     }
+    // }, [page]);
+
+    // useEffect(() => {
+    //     setIsLoading(isLoadingFetch);
+    // }, [isLoadingFetch]);
 
     return (
         <div>
             <div className='h-20'></div>
-            <Pagination
+            {/* <Pagination
                 currentPage={page}
                 totalPages={pageCount || 1}
                 setPageNumber={changePage}
-            />
+            /> */}
             {isLoading && (
-                <div className='flex justify-center items-center h-full'>
-                    <div className='max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 h-48'>
+                <div className='flex items-center justify-center h-full'>
+                    <div className='h-48 max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
                         <Loading className='w-48 h-48' />
-                        <h2 className='text-center text-4xl animate-pulse'>
+                        <h2 className='text-4xl text-center animate-pulse'>
                             Loading ...
                         </h2>
                     </div>
                 </div>
             )}
-            {!error && !isLoading && data && (
-                <ProductTileView products={data} />
+            {!error && !isLoading && data && data.listings.data && (
+                <ProductTileView products={data.listings.data} />
             )}
             {error && <div>Error: {error.message}</div>}
         </div>
