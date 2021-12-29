c49c2ab3049baf34b9a7e7f8a9f8dee2b995b9bf
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-12-29 11:19:44 -0800

    padding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ src }) => {
     return (
-        <div className='w-full aspect-video mt-16 xl:h-[48rem] overflow-hidden relative'>
+        <div className='w-full aspect-video mt-16 xl:h-[48rem] overflow-hidden relative mb-1'>
             <img
                 className='object-cover absolute inset-0 w-[200%] md:w-full aspect-auto -top-8 xl:-top-24 2xl:-top-40'
                 src={src}
